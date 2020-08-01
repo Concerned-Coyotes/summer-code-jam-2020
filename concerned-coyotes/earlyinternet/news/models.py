cb099c899c38706c081c526aa691cd1db1b37f37
MushroomMaula <maxrd79@gmail.com>
2020-08-01 19:20:43 +0200

    Add ArticleManager
    This provides and easy to use interface to convert the response of the api to the corresponding db model

--- concerned-coyotes/earlyinternet/news/models.py
+++ concerned-coyotes/earlyinternet/news/models.py
@@ -1,10 +1,36 @@
+from typing import Dict, Union
+
 from django.db import models
+from django.utils.dateparse import parse_datetime
+
+
+class ArticleManager(models.Manager):
+
+    def create_article(self, raw_article: Dict[str, Union[str, Dict[str, str]]]):
+
+        # convert timezone aware iso date string to datetime.datetime
+        published_at = parse_datetime(raw_article['publishedAt'])
+
+        article = self.create(
+            source=raw_article['source']['name'],
+            author=raw_article['author'],
+            title=raw_article['title'],
+            description=raw_article['description'],
+            content=raw_article['content'],
+            url=raw_article['url'],
+            published_at=published_at
+        )
+        return article
 
 
 class Article(models.Model):
 
+    objects = ArticleManager()
+    # now we can do
+    # >>> Article.objects.create_article({...})
+
     source = models.CharField()
-    author = models.CharField()
+    author = models.CharField(default="unknown")
     title = models.CharField()
     description = models.CharField()
     content = models.CharField(max_length=200)
