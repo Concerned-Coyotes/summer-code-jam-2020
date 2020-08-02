26469540b90130c02d0ab6a3be49a60596b97b57
MushroomMaula <maxrd79@gmail.com>
2020-08-02 18:07:04 +0200

    Add running news task on startup
    This should spin up an extra thread which will run the `python manage.py process_tasks` command and therefore run `news.tasks.get_news` every day

--- concerned-coyotes/earlyinternet/news/apps.py
+++ concerned-coyotes/earlyinternet/news/apps.py
@@ -1,5 +1,16 @@
+import threading
+
 from django.apps import AppConfig
+from django.core import management
 
 
 class NewsConfig(AppConfig):
     name = 'news'
+
+    def ready(self):
+        """ Run thread repeating the news query task """
+        background = threading.Thread(
+            target=management.call_command, args=['process_tasks'],
+            daemon=True
+        )
+        background.start()
