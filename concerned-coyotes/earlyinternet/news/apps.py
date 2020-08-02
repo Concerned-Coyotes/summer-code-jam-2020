6462a5c24de4c3e3f35a640083015d2271cb5877
MushroomMaula <maxrd79@gmail.com>
2020-08-02 21:44:02 +0200

    Run news task on startup

--- concerned-coyotes/earlyinternet/news/apps.py
+++ concerned-coyotes/earlyinternet/news/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class NewsConfig(AppConfig):
     name = 'news'
+
+    def ready(self):
+        from .tasks import get_news
+        from background_task.models import Task
+        get_news(repeat=Task.DAILY, repeat_until=None)
