a9b91a4164295dec06d3dd40d374796739ce1707
MushroomMaula <maxrd79@gmail.com>
2020-08-02 19:10:34 +0200

    Removing creation of thread
    This leads to an ValueError as django-background-tasks needs to run in the main thread

--- concerned-coyotes/earlyinternet/news/apps.py
+++ concerned-coyotes/earlyinternet/news/apps.py
@@ -1,16 +1,5 @@
-import threading
-
 from django.apps import AppConfig
-from django.core import management
 
 
 class NewsConfig(AppConfig):
     name = 'news'
-
-    def ready(self):
-        """ Run thread repeating the news query task """
-        background = threading.Thread(
-            target=management.call_command, args=['process_tasks'],
-            daemon=True
-        )
-        background.start()
