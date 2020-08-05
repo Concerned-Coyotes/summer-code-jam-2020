0489e6b2847001b279a629c2c515faefa238b086
MushroomMaula <maxrd79@gmail.com>
2020-08-05 11:19:40 +0200

    Add testing condition
    Do not run the task when tests are running as this breaks on github

--- concerned-coyotes/earlyinternet/news/apps.py
+++ concerned-coyotes/earlyinternet/news/apps.py
@@ -5,6 +5,10 @@ class NewsConfig(AppConfig):
     name = 'news'
 
     def ready(self):
+        from django.conf import settings
+        if settings.TESTING:
+            return
+
         from .tasks import get_news
         from background_task.models import Task
         get_news(repeat=Task.DAILY, repeat_until=None)
