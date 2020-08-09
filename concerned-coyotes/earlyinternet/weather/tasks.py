81dfb520cdc42c66e437ad58e4882812cd58f7ab
HealYouDown <jeremyregitz@gmail.com>
2020-08-09 12:52:45 +0200

    add a function to update weather for a specific user

--- concerned-coyotes/earlyinternet/weather/tasks.py
+++ concerned-coyotes/earlyinternet/weather/tasks.py
@@ -4,16 +4,25 @@ from .models import Weather
 from .utils import get_weather
 
 
-def fetch_weather():
+def update_weather_for_user(user: User) -> None:
+    location = user.location_set.last()
+    lat = location.latitude
+    lon = location.longitude
+
+    weather = get_weather(lat, lon)
+
+    # create weather objects
+    Weather.objects.create(
+        celsius=weather['weather']['celsius'],
+        fahrenheit=weather['weather']['fahrenheit'],
+        city=weather['city'],
+        sunrise=weather['sunrise'],
+        sunset=weather['sunset'],
+        day=weather['day']
+    )
+
+
+def fetch_weather() -> None:
     for user in User.objects.all():
-        lat, lon = user.location_set
-        weather = get_weather(lat, lon)
-        # create weather objects
-        Weather.objects.create(
-            celsius=weather['weather']['celsius'],
-            fahrenheit=weather['weather']['fahrenheit'],
-            city=weather['city'],
-            sunrise=weather['sunrise'],
-            sunset=weather['sunset'],
-            day=weather['day']
-        )
+        if user.location_set.exists():
+            update_weather_for_user(user)
