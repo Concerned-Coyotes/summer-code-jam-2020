55fc081fe8854347ab46cddbf8e7dce457801c82
MushroomMaula <maxrd79@gmail.com>
2020-08-09 13:01:59 +0200

    Add wikipedia task.
    Closes #52

--- concerned-coyotes/earlyinternet/wikipedia/apps.py
+++ concerned-coyotes/earlyinternet/wikipedia/apps.py
@@ -3,3 +3,17 @@ from django.apps import AppConfig
 
 class WikipediaConfig(AppConfig):
     name = 'wikipedia'
+
+    def ready(self):
+        from django.conf import settings
+        if settings.TESTING:
+            return
+
+        # Create the Task for fetching the news
+        # only if it does not exist yet
+        from django_q.models import Schedule
+        Schedule.objects.get_or_create(
+            func='wikipedia.tasks.fetch_wikipedia',
+            schedule_type='D',
+            repeats=-1
+        )
